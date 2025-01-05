a97bf7fd38f4b7f3f884d9102aad262a64dcd112
root <root@karan>
2025-01-05 12:39:23 +0530

    MUI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AppButton from "./Componnents/AppButton";
+import AppSidebar from "./Componnents/AppSidebar";
+import VerticalTabs from "./Componnents/VerticalTabs";
+function App() {
+  return (
+    <>
+     {/* <AppButton/> */}
+     <VerticalTabs/>
+    </>
+  );
+}
+
+export default App;
